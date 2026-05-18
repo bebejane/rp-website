f13507716fe8e5950e13d2b4f3704c300d7a487a
Bebe Jane <bjornthief@gmail.com>
2026-05-18 11:36:31 +0200

    first

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,111 +1,105 @@
-import "@/styles/index.scss";
-import s from "./layout.module.scss";
-import { apiQuery } from "next-dato-utils/api";
-import { GlobalDocument } from "@/graphql";
-import { Metadata } from "next";
-import { Icon } from "next/dist/lib/metadata/types/metadata-types";
-import { DraftModeContentLink } from "next-dato-utils/components";
+import '@/styles/index.scss';
+import s from './layout.module.scss';
+import { apiQuery } from 'next-dato-utils/api';
+import { GlobalDocument } from '@/graphql';
+import { Metadata } from 'next';
+import { Icon } from 'next/dist/lib/metadata/types/metadata-types';
+import { DraftModeContentLink } from 'next-dato-utils/components';
 
-export default async function RootLayout({ children }: LayoutProps<"/">) {
-  return (
-    <html lang="en">
-      <body id="root" className="root">
-        <main className={s.main}>{children}</main>
-        {/* <DraftModeContentLink /> */}
-      </body>
-    </html>
-  );
+export default async function RootLayout({ children }: LayoutProps<'/'>) {
+	return (
+		<html lang='en'>
+			<body id='root' className='root'>
+				<main className={s.main}>{children}</main>
+				{/* <DraftModeContentLink /> */}
+			</body>
+		</html>
+	);
 }
 
-export async function generateMetadata({
-  params,
-}: LayoutProps<"/">): Promise<Metadata> {
-  const {
-    site: { globalSeo, faviconMetaTags },
-  } = await apiQuery(GlobalDocument);
+export async function generateMetadata({ params }: LayoutProps<'/'>): Promise<Metadata> {
+	const {
+		site: { globalSeo, faviconMetaTags },
+	} = await apiQuery(GlobalDocument);
 
-  const siteName = globalSeo?.siteName ?? "";
+	const siteName = globalSeo?.siteName ?? '';
 
-  return {
-    metadataBase: new URL(process.env.NEXT_PUBLIC_SITE_URL as string),
-    icons: faviconMetaTags.map(
-      ({ attributes: { rel, sizes, type, href: url } }) => ({
-        rel,
-        url,
-        sizes,
-        type,
-      }),
-    ) as Icon[],
-    ...(await buildMetadata({
-      title: {
-        template: `${siteName} — %s`,
-        default: siteName ?? "",
-      },
-      description: globalSeo?.fallbackSeo?.description?.substring(0, 157),
-      pathname: "/",
-      image: globalSeo?.fallbackSeo?.image as FileField,
-      locale: "sv_SE",
-    })),
-  };
+	return {
+		metadataBase: new URL(process.env.NEXT_PUBLIC_SITE_URL as string),
+		icons: faviconMetaTags.map(({ attributes: { rel, sizes, type, href: url } }) => ({
+			rel,
+			url,
+			sizes,
+			type,
+		})) as Icon[],
+		...(await buildMetadata({
+			title: {
+				template: `${siteName} — %s`,
+				default: siteName ?? '',
+			},
+			description: globalSeo?.fallbackSeo?.description?.substring(0, 157),
+			pathname: '/',
+			image: globalSeo?.fallbackSeo?.image as FileField,
+			locale: 'sv',
+		})),
+	};
 }
 
 export type BuildMetadataProps = {
-  title?: string | any;
-  description?: string | null | undefined;
-  pathname?: string;
-  image?: FileField | null | undefined;
-  locale: SiteLocale;
+	title?: string | any;
+	description?: string | null | undefined;
+	pathname?: string;
+	image?: FileField | null | undefined;
+	locale: SiteLocale;
 };
 
 export async function buildMetadata({
-  title,
-  description,
-  pathname,
-  image,
+	title,
+	description,
+	pathname,
+	image,
 }: BuildMetadataProps): Promise<Metadata> {
-  description = !description
-    ? ""
-    : description.length > 160
-      ? `${description.substring(0, 157)}...`
-      : description;
-  const url = pathname
-    ? `${process.env.NEXT_PUBLIC_SITE_URL}${pathname}`
-    : undefined;
+	description = !description
+		? ''
+		: description.length > 160
+			? `${description.substring(0, 157)}...`
+			: description;
+	const url = pathname ? `${process.env.NEXT_PUBLIC_SITE_URL}${pathname}` : undefined;
 
-  return {
-    title,
-    alternates: {
-      canonical: url,
-    },
-    description,
-    openGraph: {
-      title,
-      description,
-      url,
-      images: image
-        ? [
-            {
-              url: `${image?.url}?w=1200&h=630&fit=fill&q=80`,
-              width: 800,
-              height: 600,
-              alt: title,
-            },
-            {
-              url: `${image?.url}?w=1600&h=800&fit=fill&q=80`,
-              width: 1600,
-              height: 800,
-              alt: title,
-            },
-            {
-              url: `${image?.url}?w=790&h=627&fit=crop&q=80`,
-              width: 790,
-              height: 627,
-              alt: title,
-            },
-          ]
-        : undefined,
-      locale: "sv_SE",
-      type: "website",
-    },
-  };
+	return {
+		title,
+		alternates: {
+			canonical: url,
+		},
+		description,
+		openGraph: {
+			title,
+			description,
+			url,
+			images: image
+				? [
+						{
+							url: `${image?.url}?w=1200&h=630&fit=fill&q=80`,
+							width: 800,
+							height: 600,
+							alt: title,
+						},
+						{
+							url: `${image?.url}?w=1600&h=800&fit=fill&q=80`,
+							width: 1600,
+							height: 800,
+							alt: title,
+						},
+						{
+							url: `${image?.url}?w=790&h=627&fit=crop&q=80`,
+							width: 790,
+							height: 627,
+							alt: title,
+						},
+					]
+				: undefined,
+			locale: 'sv_SE',
+			type: 'website',
+		},
+	};
 }
