fbb78bcb55b831861fe67b2bf9eef2e935d66612
Bebe Jane <bjornthief@gmail.com>
2026-05-18 12:03:37 +0200

    p

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,46 @@
-import s from "./page.module.scss";
-import { AllPostsDocument, PostDocument } from "@/graphql";
-import { apiQuery } from "next-dato-utils/api";
-import { DraftMode } from "next-dato-utils/components";
-import { notFound } from "next/navigation";
-import { Markdown } from "next-dato-utils/components";
-import { Image } from "react-datocms";
-import Content from "@/components/content/Content";
+import s from './page.module.scss';
+import { StartDocument } from '@/graphql';
+import { apiQuery } from 'next-dato-utils/api';
+import { DraftMode } from 'next-dato-utils/components';
+import { notFound } from 'next/navigation';
+import { Markdown } from 'next-dato-utils/components';
+import { Image } from 'react-datocms';
+import Content from '@/components/content/Content';
 
-export default async function Home({ params }: PageProps<"/">) {
-  //if (!post) return notFound();
+export default async function Home({ params }: PageProps<'/'>) {
+	const { start, draftUrl } = await apiQuery(StartDocument);
 
-  return (
-    <>
-      <article>Hem</article>
-      {/* <DraftMode url={draftUrl} path={`/${locale}/posts/${slug}`} /> */}
-    </>
-  );
+	if (!start) return notFound();
+
+	return (
+		<>
+			<article>
+				<h1>Välkommen till Red Pharalope</h1>
+				<section>
+					<Content content={start.text} />
+				</section>
+				<section>
+					<h2>Ladda ner våra appar</h2>
+					<ul>
+						<li>
+							<div></div>
+						</li>
+					</ul>
+				</section>
+			</article>
+			<DraftMode url={draftUrl} path={`/`} />
+		</>
+	);
 }
 
-export async function generateStaticParams({ params }: PageProps<"/">) {
-  // const { allPosts } = await apiQuery(AllPostsDocument, {
-  // 	variables: {
-  // 		locale: locale as SiteLocale,
-  // 	},
-  // 	all: true,
-  // });
-  // return allPosts.map((post) => ({
-  // 	post: post.slug,
-  // }));
+export async function generateStaticParams({ params }: PageProps<'/'>) {
+	// const { allPosts } = await apiQuery(AllPostsDocument, {
+	// 	variables: {
+	// 		locale: locale as SiteLocale,
+	// 	},
+	// 	all: true,
+	// });
+	// return allPosts.map((post) => ({
+	// 	post: post.slug,
+	// }));
 }
